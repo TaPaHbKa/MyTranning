f005518d95cb169f4c83493b45989a72779fb526
TaPaHbKa <tarantul267@gmail.com>
2024-11-02 15:51:14 +0300

    1

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+number_of_homework = 12
+number_of_hours_spend = 1.5
+course_name = "Piton"
+print ("Курс:", course_name, ", всего задач:", number_of_homework, ", затрачено часов:", number_of_hours_spend,
+       ", среднее время выполнения:", number_of_hours_spend/number_of_homework )
