4a31eda895d558d891bc2ffe35528bf02604e58a
TaPaHbKa <tarantul267@gmail.com>
2024-11-02 18:48:09 +0300

    1

---
+++ module_1_17.py
@@ -0,0 +1,9 @@
+immutable_var = 12, 4, "Five", True
+print (immutable_var)
+#immutable_var [1] = 2
+#Сам по себе кортеж неизменяемое хранилище данных
+mutable_list = ["Fire", 3.5, False, 8]
+mutable_list [0] = True
+mutable_list [2] = "Seven"
+mutable_list.append(45)
+print(mutable_list )
