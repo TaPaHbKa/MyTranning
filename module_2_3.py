e2e1317b27145207319ecf6b7b60688a7967d46c
TaPaHbKa <tarantul267@gmail.com>
2024-11-06 17:57:07 +0300

    1

---
+++ module_2_3.py
@@ -0,0 +1,9 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = my_list [4]
+print ('Вы видите список чисел', my_list)
+b = (int(input('Введите число из списка ')))
+while b is len(my_list):
+    if b is my_list:
+        print ('Вы ввели число не из списка')
+    else :
+        break
