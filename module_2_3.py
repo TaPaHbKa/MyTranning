dc3da499c4def6261856de46be88d912f7039809
TaPaHbKa <tarantul267@gmail.com>
2024-11-11 23:04:54 +0300

    2

--- module_2_3.py
+++ module_2_3.py
@@ -1,9 +1,10 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-a = my_list [4]
-print ('Вы видите список чисел', my_list)
-while True:
-    b = int(input('Введите число из списка: '))
-    if a < len(my_list) and b >= a:
-        continue
+a = 0
+while a < len(my_list):
+    if my_list[a] > 0:
+        print(my_list[a])
+        a += 1
+    elif my_list[a] == 0:
+        a += 1
     else:
         break
