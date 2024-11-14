34cdd840a9eb4fff82c38127ae245eaa5001c092
TaPaHbKa <tarantul267@gmail.com>
2024-11-14 18:56:47 +0300

    1

---
+++ module_2_5.py
@@ -0,0 +1,17 @@
+from multiprocessing.managers import Value
+
+
+def get_matrix(n, m, value):
+    matrix = []
+    for line in range(n):
+        car = []
+        for column in range(m):
+            car.append(value)
+        matrix.append(car)
+    return matrix
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
