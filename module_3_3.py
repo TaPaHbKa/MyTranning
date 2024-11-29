f3adf7672bc7347a83d7b631a1836f917ec7ca5e
TaPaHbKa <tarantul267@gmail.com>
2024-11-29 16:08:59 +0300

    1

---
+++ module_3_3.py
@@ -0,0 +1,11 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+values_list = ['a', 3, True]
+values_dict = {'a': 3, 'b': 'b', 'c': False}
+values_list_2 = [54.32, 'car']
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
+print_params(*values_list)
+print_params(**values_dict)
+print_params(*values_list_2, 42)
